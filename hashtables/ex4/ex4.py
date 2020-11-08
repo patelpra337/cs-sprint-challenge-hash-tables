5df39193599fb90b34b5529e56c0e7244be2edf5
Pravin Patel <patelpra337@gmail.com>
2020-11-08 15:17:19 -0600

    update ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -8,7 +8,14 @@ def has_negatives(a):
     for num in a:
         abs_num = abs(num)
         if abs_num in nums_dict:
-            value = nums_dict
+            value = nums_dict[abs_num]
+            if value == 0:
+                continue
+            elif value + num == 0:
+                result.append(abs_num)
+                nums_dict[abs_num] = 0
+        else:
+            nums_dict[abs_num] = num
 
     return result
 
